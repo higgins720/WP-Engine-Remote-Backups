3aba332d7422acd724d4b591736be108dd014bdb
Jesse Higgins <86027173+higgins720@users.noreply.github.com>
2022-11-19 13:42:35 -0700

    Cleaned up python script, got rid of bash script.

--- BackupWPEsite.py
+++ BackupWPEsite.py
@@ -1,32 +1,78 @@
 import requests
-import os
+import os.path
 import json
 
-installsURL = "https://api.wpengineapi.com/v1/installs?limit=10"
+backupName = 'wisdomecare'
 
-sitesURL = "https://api.wpengineapi.com/v1/sites?limit=10"
+nameRecordDictionary = {}
+apiKey = []
 
-backupjson = {
-    "description": "Updates Backup",
-    "notification_emails": [
-        "jesse@csdesignstudios.com"
-    ]
-}
+# Check for API Keys
+# Todo: have better security here
+if (os.path.exists('keys.json')):
+    with open('keys.json') as json_file:
+        keysObject = json.load(json_file)
+        for key, value in keysObject.items():
+            apiKey.append(value)
+else:
+    # If no key file exists, write a new one with creds
+    keyDict = {}
+    key1 = input("Enter API Key 1/2: ")
+    apiKey.append(key1)
+    keyDict[0] = key1
+    key2 = input("Enter API Key 2/2: ")
+    apiKey.append(key2)
+    keyDict[1] = key2
+    with open('keys.json', 'w', encoding='utf-8') as key_file:
+        json.dump(keyDict, key_file)
 
-# API Keys
-# 0c27ac7e-b74d-4e8d-b469-a9304b4e3368 : zzc4M8UuAh49S7vjREYMa1O58vLliESl
-response = requests.get(sitesURL,
-    auth=requests.auth.HTTPBasicAuth(
-    '0c27ac7e-b74d-4e8d-b469-a9304b4e3368',
-    'zzc4M8UuAh49S7vjREYMa1O58vLliESl'))
-
-j = response.json()
-envName = 'loantucson'
-#envId = j['results'][0][envName]['id']
-print(j)
-
-#print(response.text)
-
-def run_backup(envId):
+# Get a list of all environments in the system
+# Rather than compiling a list every time, reference json file if it exists, if not then write one.
+if (os.path.exists('installs.json')):
+    # fill dictionary with json file data
+    with open('installs.json') as json_file:
+        jsonFile = json.load(json_file)
+        #print("JSON results: " + str(len(object["results"])))
+        print("Loading Installs...")
+        for key, value in jsonFile.items():
+            nameRecordDictionary[key] = value
+else:
+    print("Compiling list of environments...")
+    nextURL = 'https://api.wpengineapi.com/v1/installs'
+    while nextURL is not None:
+        installsResponse = requests.get(nextURL, auth=requests.auth.HTTPBasicAuth(apiKey[0], apiKey [1]), timeout=10)
+        object = json.loads(installsResponse.text)
+        print("HTTP results: " + str(len(object["results"])))
+        nextURL = object["next"]
+        print(object["count"])
+        print(object["next"])
     
-    requests.post('https://api.wpengineapi.com/v1/installs/' + envId + '/backups', json = myobj)
+        for record in object["results"]:
+            nameRecordDictionary[record["name"]] = record
+
+        with open('installs.json', 'w', encoding='utf-8') as f:
+            json.dump(nameRecordDictionary, f)
+
+# Now that we know the dictionary is populated, we can store values
+installID = nameRecordDictionary[backupName]["id"]
+
+def run_backup(install_ID, name):
+    # 'https://api.wpengineapi.com/v1/installs'
+    backupURL = 'https://api.wpengineapi.com/v1/installs/' + install_ID + '/backups'
+    #print(backupURL)
+    data = {"description": "Taking a backup of " + name, "notification_emails": ["jesse@csdesignstudios.com"]}
+    backupResponse = requests.post(backupURL, auth=requests.auth.HTTPBasicAuth(apiKey[0], apiKey[1]), timeout=100, json=data)
+    print(backupResponse.text)
+    object = json.loads(backupResponse.text)
+    #print(object["id"])
+    return object["id"]
+
+# This just returns the phrase "Requested"
+def backup_status(install_ID, backup_ID):
+    backupStatusURL = 'https://api.wpengineapi.com/v1/installs/' + install_ID + '/backups/' + backup_ID
+    print(backupStatusURL)
+    backupStatusResponse = requests.get(backupStatusURL, auth=requests.auth.HTTPBasicAuth(apiKey[0], apiKey[1]), timeout=10)
+    print(backupStatusResponse.text)
+
+backupID = run_backup(installID, backupName)
+#backup_status(installID, backupID)
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # WP Engine Remote Backup
 Initiates a backup of a given site on WP Engine
+
+## Goals
+
+My vision for this is to be able to provide a list of environments by name, step through each one and:
+
+1. Run a backup of the site, and wait until backup is completed before next step. 
+2. Check for WP Core update if available, update if it is. 
+3. Check for plugin updates, update all available. 
+4. Check for theme update, update if it can. 
+5. Check site status, 
+6. If site gets 404 error, provide link and option to restore backup. 
+7. Prompt user to continue to next site
+
+This will probably change in the future, I'll probably want to modify things and add features down the road. But if I can get this to work and make it easy for someone else to use on their machine without any reconfiguring, besides the inital setup of providing necessary authentication and the list of sites to iterate through.
+
+## Features of the Future
+
+### Store Private Data Locally
+Write a hidden, encrypted file with api-key, as well as a seperate file of the list of sites with all the important information. Then, the program will retrieve that data as needed. I should do this before uploading to a public repository on GitHub.
+
+### Restore from Backup
+There has to be a way to use the backup to restore the site, rather than going in manually to WPE engine and clicking "restore". I'll have to just figure that out.
+
+### Schedule updates
+For instance, schedule all sites to be ran through at 1am. This might be nice to have, if I can automatically run a site-restore after the program detects serious issues have occurred. That way you don't have to wake up the next day and find all the sites broken, or nothing was updated because it stopped after the first site had an issue.
